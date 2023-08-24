e5500ce0a11ad17179a1d7ac52c8b668e8c9d20e
SULTANUM MOBIN <119931140+smsultan76@users.noreply.github.com>
2023-08-24 09:54:49 +0600

    Create  1114 - Fixed Password.c
    Write a program that keep reading a password until it is valid. For each wrong password read, write the message "Senha inválida". When the password is typed correctly print the message "Acesso Permitido" and finished the program. The correct password is the number 2002.
    
    Input
    The input file contains several tests cases. Each test case contains only an integer number.
    
    Output
    For each number read print a message corresponding to the description of the problem.

---
+++ Beecrowd/C/1114 - Fixed Password.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int X;
+    while (1)
+    {
+        scanf("%d",&X);
+        if(X==2002){
+            printf("Acesso Permitido\n");
+            break;
+        }
+        else printf("Senha Invalida\n");
+    }
+}
